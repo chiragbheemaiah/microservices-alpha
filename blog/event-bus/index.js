8115d3bd85817dd1b51403523a13c0d191e49c67
chiragbheemaiah <chirag.bheemaiah@gmail.com>
2024-07-28 18:58:13 -0400

    implemented a more robust event-bus which stores all events for services to use once they are up

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,8 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
+    console.log(events);
     // post service
     axios.post('http://localhost:4000/events', event).catch((error) => console.log(error));
     // comments service
@@ -18,6 +23,10 @@ app.post('/events', (req, res) => {
 
 
     res.send( {status: 'OK'} );
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
--- blog/query/index.js
+++ blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,12 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const {type, data} = req.body;
+const handleEvent = (type, data) => {
     if(type === 'PostCreated'){
         const {id, title} = data;
         posts[id] = {id, title, comments: []}
@@ -30,9 +26,24 @@ app.post('/events', (req, res) => {
         comment.content = content;
         comment.status = status;
     }
+}
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const {type, data} = req.body;
+    handleEvent(type, data);
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on port 4002');
+    // Run only on first boot
+    const res = await axios.get('http://localhost:4005/events').catch((error) => console.log(error));
+    console.log(res);
+    for(let event of res.data){
+        console.log('Processing event: ', event.type);
+        handleEvent(event.type, event.data);
+    }
 });
